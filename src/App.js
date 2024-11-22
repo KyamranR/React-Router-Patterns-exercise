905a966cd90b9bf50e92ef8c18eee617095de2d7
Kyamran <kyamranr@gmail.com>
2024-11-21 19:32:22 -0500

    modified app.js to add colors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Nav from "./Nav";
 import DogList from "./DogList";
@@ -6,16 +6,43 @@ import DogDetails from "./DogDetails";
 import whiskey from "./images/whiskey.jpg";
 import duke from "./images/duke.jpg";
 import perry from "./images/perry.jpg";
+
+import Colors from "./components/Colors";
+import ColorDetail from "./components/ColorDetail";
+import NewColorForm from "./components/NewColorForm";
+
 import "./App.css";
 
 function App({ dogs }) {
+  const [colors, setColors] = useState({
+    red: "#FF0000",
+    green: "#00FF00",
+    blue: "#0000FF",
+  });
+
+  const addColor = (newColor) =>
+    setColors((colors) => ({ ...newColor, ...colors }));
+
   return (
     <div className="App">
       <BrowserRouter>
         <Nav dogs={dogs} />
         <Routes>
+          {/* Dogs Finder Routes */}
           <Route path="/dogs" element={<DogList dogs={dogs} />} />
           <Route path="/dogs/:name" element={<DogDetails dogs={dogs} />} />
+
+          {/* Color Factory Routes */}
+          <Route path="/colors" element={<Colors colors={colors} />} />
+          <Route
+            path="/colors/new"
+            element={<NewColorForm addColor={addColor} />}
+          />
+          <Route
+            path="/colors/:color"
+            element={<ColorDetail colors={colors} />}
+          />
+
           <Route path="*" element={<Navigate to="/dogs" />} />
         </Routes>
       </BrowserRouter>
